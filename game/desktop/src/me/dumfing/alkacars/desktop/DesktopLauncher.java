80ad2f0900155dcb502822b19168d13f52c033bf
Dumfing <dumpling.d.m@gmail.com>
2017-09-08 10:31:41 -0400

    first collab commit

---
+++ game/desktop/src/me/dumfing/alkacars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.dumfing.alkacars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.dumfing.alkacars.Alkacars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Alkacars(), config);
+	}
+}
